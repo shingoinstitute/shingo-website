2f745596c393cacfd6f0bca46e254437664f9373
Case Haws <case.haws@aggiemail.usu.edu>
2016-05-24 12:37:10 -0600

    Speakers populated from SF

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 require('dotenv').load();
 
-var express      = require('express')
-  , exphbs       = require('express-handlebars')
-  , path         = require('path')
-  , favicon      = require('serve-favicon')
-  , logger       = require('morgan')
-  , cookieParser = require('cookie-parser')
-  , bodyParser   = require('body-parser')
-  , sass         = require('node-sass')
-  , config       = require('./config.js')
+var express = require('express'),
+    exphbs = require('express-handlebars'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    sass = require('node-sass'),
+    config = require('./config.js')
 
 var languageTree = require('./routes/tree.js');
 
@@ -33,15 +33,19 @@ var app = express();
 app.set('port', config.port)
 
 // Setup View Engine
-app.set('views', path.join(__dirname,  'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'base'}));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'base'
+}));
 app.set('view engine', 'handlebars');
 
 //TODO Fix favicon
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -49,34 +53,34 @@ app.use('/', languageTree);
 
 /*   Error Handling  */
 //Catch 404 and forward
-app.use(function(req, res, next){
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // Dev Error: Prints stacktrace
-if(app.get('env') === 'development'){
-  app.use(function(err, req, res, next){
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // Production Error: No stacktrace leaked to user
-app.use(function(err, req, res, next){
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
-app.listen(app.get('port'), function(){
-  console.log('Node is on port', app.get('port'));
+app.listen(app.get('port'), function() {
+    console.log('Node is on port', app.get('port'));
 })
 
 module.exports = app;
--- routes/en/index-en.js
+++ routes/en/index-en.js
@@ -1,7 +1,7 @@
-var express = require('express')
-  , Promise      = require('bluebird')
-  , SF           = Promise.promisifyAll(require('../../models/sf'))
-  , router       = express.Router()
+var express = require('express'),
+    Promise = require('bluebird'),
+    SF = Promise.promisifyAll(require('../../models/sf')),
+    router = express.Router()
 
 var routes_affiliates = require('./index-affiliates.js');
 var routes_recipients = require('./index-recipients.js');
@@ -9,95 +9,146 @@ var routes_recipients = require('./index-recipients.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shingo Institute - Leading a New Era of Enterprise Excellence'});
+    res.render('index', {
+        title: 'Shingo Institute - Leading a New Era of Enterprise Excellence'
+    });
 });
 
 
-                      /* Education Menu */
+/* Education Menu */
 /* GET model  */
 router.get('/model', function(req, res, next) {
-  res.render('education/model', { title: 'The Shingo Model - Shingo Institute' });
+    res.render('education/model', {
+        title: 'The Shingo Model - Shingo Institute'
+    });
 });
 
 /* GET education */
 router.get('/education', function(req, res, next) {
-    var ws_query  = 'SELECT Id, Name, Organizing_Group__r.Name, Organizing_Group__r.Page_Path__c, Course__c, Event_Start_Date__c, Event_End_Date__c, Host_Organization__c, Host_City__c, Host_Country__c, Event_Website__c FROM Event__c WHERE Visibility__c=\'Public\' AND Event_Type__c=\'Affiliate Event\' AND Verified__c=true AND Course__c!=null AND Event_End_Date__c>=YESTERDAY AND Event_Status__c=\'Active event\' ORDER BY Event_Start_Date__c';
+    var ws_query = 'SELECT Id, Name, Organizing_Group__r.Name, Organizing_Group__r.Page_Path__c, Course__c, Event_Start_Date__c, Event_End_Date__c, Host_Organization__c, Host_City__c, Host_Country__c, Event_Website__c FROM Event__c WHERE Visibility__c=\'Public\' AND Event_Type__c=\'Affiliate Event\' AND Verified__c=true AND Course__c!=null AND Event_End_Date__c>=YESTERDAY AND Event_Status__c=\'Active event\' ORDER BY Event_Start_Date__c';
 
     var query_res = {
-      "Discover": new Array(),
-      "Enable": new Array(),
-      "Improve": new Array(),
-      "Align": new Array()
+        "Discover": new Array(),
+        "Enable": new Array(),
+        "Improve": new Array(),
+        "Align": new Array()
     }
 
     SF.queryAsync(ws_query)
-    .then(function(results){
-      var events = results.records
-      for(var i in events){
-        query_res[events[i].Course__c].push(events[i])
-      }
-      // console.log(JSON.stringify(query_res,null,4));
-      res.render('education/education', { title: 'Education - Shingo Institute', workshops: query_res });
-    }).catch(function(err){
-      console.log("sf.js:Line 40 " + err)
-      res.render('education/education', { title: 'Education - Shingo Institute', workshops: query_res });
-    })
+        .then(function(results) {
+            var events = results.records
+            for (var i in events) {
+                query_res[events[i].Course__c].push(events[i])
+            }
+            // console.log(JSON.stringify(query_res,null,4));
+            res.render('education/education', {
+                title: 'Education - Shingo Institute',
+                workshops: query_res
+            });
+        }).catch(function(err) {
+            console.log("sf.js:Line 40 " + err)
+            res.render('education/education', {
+                title: 'Education - Shingo Institute',
+                workshops: query_res
+            });
+        })
 
 });
 
-            /*  Conference, Summits & Study Tour */
+/*  Conference, Summits & Study Tour */
 /* GET manufacturing  */
 router.get('/events/manufacturing', function(req, res, next) {
-  res.render('conference/manufacturing', { layout: 'summit', title: 'Manufacturing Summit - Shingo Institute' });
+    var query = "SELECT Id, Speaker_Contact__r.Name, Speaker_Type__c, Speaker_Contact__r.Account.Name, Biography__c, Speaker_Contact__r.Title, Speaker_Image__c FROM Speaker__c WHERE Event__c='a0R1200000UmjyDEAR' ORDER BY Speaker_Contact__r.LastName"
+    SF.queryAsync(query).then(function(event) {
+        var keynote = new Array()
+        var concurrent = new Array()
+        for(var i = 0; i < event.records.length; i++){
+          console.log(event.records[i].Speaker_Type__c);
+          if(event.records[i].Speaker_Type__c == 'Keynote Speaker'){
+            keynote.push(event.records[i])
+          }
+          else {
+            concurrent.push(event.records[i])
+          }
+        }
+        res.render('conference/manufacturing', {
+            layout: 'summit',
+            title: 'Manufacturing Summit - Shingo Institute',
+            keynote: keynote,
+            concurrent: concurrent
+        });
+    }).catch(function(err){
+      console.log("Line 69: " + err);
+    })
 });
 
 /* GET europe  */
 router.get('/events/europe', function(req, res, next) {
-  res.render('conference/europe', { layout: 'summit', title: 'European Conference - Shingo Institute' });
+    res.render('conference/europe', {
+        layout: 'summit',
+        title: 'European Conference - Shingo Institute'
+    });
 });
 
 /* GET latinamerica  */
 router.get('/events/latinamerica', function(req, res, next) {
-  res.render('conference/latinamerica', { layout: 'summit', title: 'Congreso Latin America - Shingo Institute' });
+    res.render('conference/latinamerica', {
+        layout: 'summit',
+        title: 'Congreso Latin America - Shingo Institute'
+    });
 });
 
 
 /* GET Japan studytour */
 router.get('/japanstudytour', function(req, res, next) {
-  res.render('education/japanstudytour', { title: 'Study Tour - Shingo Institute' });
+    res.render('education/japanstudytour', {
+        title: 'Study Tour - Shingo Institute'
+    });
 });
 
 /* GET USA studytour */
 router.get('/usastudytour', function(req, res, next) {
-  res.render('education/usastudytour', { title: 'Study Tour - Shingo Institute' });
+    res.render('education/usastudytour', {
+        title: 'Study Tour - Shingo Institute'
+    });
 });
 
-                        /* LEAN Network */
+/* LEAN Network */
 /*GET LEAN Home Page*/
 router.get('/teaching-lean', function(req, res, next) {
-  res.render('./LEAN/teaching-lean', { title: 'Lean Education Academic Network - Shingo Institute' });
+    res.render('./LEAN/teaching-lean', {
+        title: 'Lean Education Academic Network - Shingo Institute'
+    });
 });
 
 /*GET LEAN Resource Page*/
 router.get('/teaching-lean/resources', function(req, res, next) {
-  res.render('./LEAN/resources', { title: 'Resources - Lean Education Academic Network - Shingo Institute' });
+    res.render('./LEAN/resources', {
+        title: 'Resources - Lean Education Academic Network - Shingo Institute'
+    });
 });
 
 /*GET LEAN Events Page*/
 router.get('/teaching-lean/events', function(req, res, next) {
-  res.render('./LEAN/events', { title: 'Events - Lean Education Academic Network - Shingo Institute' });
+    res.render('./LEAN/events', {
+        title: 'Events - Lean Education Academic Network - Shingo Institute'
+    });
 });
 
 /*GET LEAN Professors Page*/
 router.get('/teaching-lean/professors', function(req, res, next) {
-  res.render('./LEAN/professors', { title: 'Professors - Lean Education Academic Network - Shingo Institute' });
+    res.render('./LEAN/professors', {
+        title: 'Professors - Lean Education Academic Network - Shingo Institute'
+    });
 });
 
 
-                       /*  Awards Route */
+/*  Awards Route */
 /* GET challengefortheprize */
 router.get('/challengefortheprize', function(req, res, next) {
-  res.render('awards/challengefortheprize', { title: 'The Shingo Prize - Shingo Institute' });
+    res.render('awards/challengefortheprize', {
+        title: 'The Shingo Prize - Shingo Institute'
+    });
 });
 
 /* GET awards routes */
@@ -105,77 +156,95 @@ router.use('/awards', routes_recipients);
 
 /* GET researchaward  */
 router.get('/researchaward', function(req, res, next) {
-  res.render('awards/researchaward', { title: 'Research Publication Award - Shingo Institute' });
+    res.render('awards/researchaward', {
+        title: 'Research Publication Award - Shingo Institute'
+    });
 });
 
 // TODO: Add Affiliates Route based on url param to render specific awards page content. Fetch Content from SF
 
 
-                       /*  Affiliates Route */
+/*  Affiliates Route */
 /* GET affiliates */
 router.use('/affiliates', routes_affiliates);
 
 
-                        /*  About Menu  */
+/*  About Menu  */
 // TODO: Pages contain content that can be populated from salesforce
 
 /* GET about */
 router.get('/about', function(req, res, next) {
-  res.render('about/about', { title: 'Mission & History - Shingo Institute' });
+    res.render('about/about', {
+        title: 'Mission & History - Shingo Institute'
+    });
 });
 
 /* GET academy */
 router.get('/academy', function(req, res, next) {
-  res.render('about/academy', { title: 'Shingo Academy - Shingo Institute' });
+    res.render('about/academy', {
+        title: 'Shingo Academy - Shingo Institute'
+    });
 });
 
 /* GET examiner */
 router.get('/examiners', function(req, res, next) {
-  res.render('about/examiners', { title: 'Examiners - Shingo Institute' });
+    res.render('about/examiners', {
+        title: 'Examiners - Shingo Institute'
+    });
 });
 
 /* GET seab */
 router.get('/seab', function(req, res, next) {
-  res.render('about/seab', { title: 'Shingo Executive Advisory Board - Shingo Institute' });
+    res.render('about/seab', {
+        title: 'Shingo Executive Advisory Board - Shingo Institute'
+    });
 });
 
 /* GET shingoteam */
 router.get('/shingoteam', function(req, res, next) {
-  res.render('about/shingoteam', { title: 'The Shingo Team - Shingo Institute' });
+    res.render('about/shingoteam', {
+        title: 'The Shingo Team - Shingo Institute'
+    });
 });
 
 
-                          /*Other Routes*/
+/*Other Routes*/
 /* GET research - Linked from homepage only */
 router.get('/research', function(req, res, next) {
-  res.render('about/research', { title: 'Insight - Shingo Institute' });
+    res.render('about/research', {
+        title: 'Insight - Shingo Institute'
+    });
 });
 
 /* GET privacy policy - Linked from homepage only */
 router.get('/privacy-policy', function(req, res, next) {
-  res.render('about/privacy-policy', { title: 'Privacy Policy - Shingo Institute' });
+    res.render('about/privacy-policy', {
+        title: 'Privacy Policy - Shingo Institute'
+    });
 });
 
 /* GET Prize FAQ */
 router.get('/prizefaq', function(req, res, next) {
-  res.render('about/prizefaq', { title: 'Prize Questions - Shingo Institute' });
+    res.render('about/prizefaq', {
+        title: 'Prize Questions - Shingo Institute'
+    });
 });
 
 
-                          /*Files*/
+/*Files*/
 /* GET .pdf - From challengefortheprize */
 router.get('/assets/Assessment_Process_Flow_Chart.pdf', function(req, res, next) {
-  res.send('../assets/Assessment_Process_Flow_Chart.pdf');
+    res.send('../assets/Assessment_Process_Flow_Chart.pdf');
 });
 
 /* GET .pdf - From challengefortheprize*/
 router.get('/assets/Application_Guidelines.pdf', function(req, res, next) {
-  res.send('../assets/Application_Guidelines.pdf');
+    res.send('../assets/Application_Guidelines.pdf');
 });
 
 /* GET .pdf - From researchaward*/
 router.get('/assets/Research-Award-Application-Form.pdf', function(req, res, next) {
-  res.send('../assets/Research-Award-Application-Form.pdf');
+    res.send('../assets/Research-Award-Application-Form.pdf');
 });
 
 
