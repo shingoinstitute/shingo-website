e482f2e9b3a31ffd025b28f90965880054a6c085
sysMachinist <case.haws@usu.edu>
2016-08-16 21:10:48 -0600

    Final updates to index summit logos

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ app.set('view engine', 'handlebars');
 
 
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
-//app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
@@ -165,16 +165,16 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-// // Dev Error: Prints stacktrace
-// if (app.get('env') === 'development') {
-//     app.use(function(err, req, res, next) {
-//         res.status(err.status || 500);
-//         res.render('error', {
-//             message: err.message,
-//             error: err
-//         });
-//     });
-// }
+// Dev Error: Prints stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
 
 // Production Error: No stacktrace leaked to user
 app.use(function(err, req, res, next) {
